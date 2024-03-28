52c8d6a550a4d03a55dae631d4c9440a6dbe26ec
Crysdale <crysdale@etsu.edu>
2024-03-28 12:37:30 -0400

    Obscure Phrase and Guest Login Removed
    "Bucs" is the answer (case insensitive)

--- BucStop/Controllers/AccountController.cs
+++ BucStop/Controllers/AccountController.cs
@@ -8,7 +8,7 @@ namespace BucStop.Controllers
 {
     public class AccountController : Controller
     {
-        public string email { get; set; } = string.Empty;
+        public string obscurePhrase { get; set; } = string.Empty;
 
         [AllowAnonymous]
         public IActionResult Login()
@@ -18,14 +18,15 @@ namespace BucStop.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public async Task<IActionResult> Login(string email)
+        
+        public async Task<IActionResult> Login(string obscurePhrase)
         {
-            if (Regex.IsMatch(email, @"\b[A-Za-z0-9._%+-]+@etsu\.edu\b"))
+            if (Regex.IsMatch(obscurePhrase, @"BUCS", RegexOptions.IgnoreCase))
             {
                 // If authentication is successful, create a ClaimsPrincipal and sign in the user
                 var claims = new[]
                 {
-                    new Claim(ClaimTypes.Name, email),
+                    new Claim(ClaimTypes.Name, obscurePhrase),
                     new Claim(ClaimTypes.NameIdentifier, "user_id"),
                 };
 
@@ -40,32 +41,11 @@ namespace BucStop.Controllers
             else
             {
                 // Authentication failed, return to the login page with an error message
-                ModelState.AddModelError(string.Empty, "Only ETSU students can play, sorry :(");
+                ModelState.AddModelError(string.Empty, "Incorrect phrase. Make sure there is not exclamation point.");
                 return View();
             }
         }
-        /// <summary>
-        /// GuestLogin generates a blank email to allow guests to play games without signing into a personal account.
-        /// </summary>
-        /// <returns></returns>
-        public async Task<IActionResult> GuestLogin()
-        {
-            //Generates a blank string that is used as a guest login placeholder.
-            string email = string.Empty;
-            var claims = new[]
-            {
-                    new Claim(ClaimTypes.Name, email),
-                    new Claim(ClaimTypes.NameIdentifier, "user_id"),
-            };
-
-            var claimsIdentity = new ClaimsIdentity(claims, "custom");
-            var userPrincipal = new ClaimsPrincipal(claimsIdentity);
-
-            // Sign in the user
-            await HttpContext.SignInAsync("CustomAuthenticationScheme", userPrincipal);
-            return RedirectToAction("Index", "Games");
-            //This method was pulled from the original login method above. RegEx check was removed.
-        }
+        
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync("CustomAuthenticationScheme");
